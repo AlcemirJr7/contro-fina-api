914aa225186b89b007697efd27bfd3e3c47d403d
ALCEMIR BELINSKI JUNIOR <68364259+AlcemirJr7@users.noreply.github.com>
2025-07-14 12:35:50 -0300

    Remove postgre timestamp config AppDbContext
    Remove postgre timestamp config AppDbContext

--- ControlFina.Infrastructure/Database/AppDbContext.cs
+++ ControlFina.Infrastructure/Database/AppDbContext.cs
@@ -4,17 +4,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ControlFina.Infrastructure.Database;
 
-public class AppDbContext : DbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-    {
-        // Enable legacy timestamp behavior for PostgreSQL compatibility
-        // This is necessary to handle DateTime values correctly in PostgreSQL
-        // when using Npgsql, especially for older versions or specific configurations.
-        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-        AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
-    }
-
     public DbSet<Category> Categories { get; set; }
     public DbSet<Transaction> Transactions { get; set; }
 
